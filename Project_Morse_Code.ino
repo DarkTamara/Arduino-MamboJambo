5279d00a05b8ff7b3affe2ac244b0f58fc66f6bb
DarkTamara <38437336+DarkTamara@users.noreply.github.com>
2018-06-16 16:43:55 +0300

    Add files via upload

---
+++ Project_Morse_Code.ino
@@ -0,0 +1,162 @@
+int Dot=250;
+int Line=700;
+int SmallSpace=700;
+int BigSpace=1200;
+int Forever=999999;
+int LEDPin=13;
+
+void setup() {
+  pinMode(LEDPin, OUTPUT);
+
+}
+
+void loop() {
+  //I
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin,HIGH);//dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+  
+  delay(BigSpace);//stahp it
+
+
+  //L
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+
+    delay(BigSpace);//stahp it
+
+  //O
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+
+  delay(BigSpace);//stahp it
+
+  //V
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+  
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+  
+  delay(BigSpace);//stahp it
+
+  //E
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+
+  delay(BigSpace);//stahp it
+
+  //Y
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+  
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+  
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+
+  delay(BigSpace);//stahp it
+
+  //O
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+
+  delay(BigSpace);//stahp it
+
+  //U
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+  
+  digitalWrite(LEDPin,HIGH); //dot
+  delay(Dot); 
+  digitalWrite(LEDPin,LOW); 
+  delay(SmallSpace);
+
+  digitalWrite(LEDPin, HIGH);//line
+  delay(Line);
+  digitalWrite(LEDPin, LOW);
+  delay(SmallSpace);
+
+
+  digitalWrite(LEDPin, LOW);
+  delay(Forever);
+  
+}
+  
