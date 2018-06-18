4af93139981b191d4135393ad5c19703d5714af7
DarkTamara <38437336+DarkTamara@users.noreply.github.com>
2018-06-18 17:50:29 +0300

    Add files via upload

---
+++ Day2.ino
@@ -0,0 +1,18 @@
+int LED=9;//ir led
+int TimeOn=900;
+int TimeOff=900;
+
+void setup() {
+
+  pinMode(LED, OUTPUT);//
+
+}
+
+void loop() {
+
+  digitalWrite(LED, HIGH);//turns the light on
+  delay(TimeOn);
+  digitalWrite(LED, LOW);
+  delay(TimeOff);
+  
+}
---
+++ MultiLEDfest.ino
@@ -0,0 +1,27 @@
+  int FirstLED=9;//ir led
+  int SecLED=10;
+  int TimeOn=900;
+  int TimeOff=100;
+  int TimeON=100;
+  int TimeOFF=900;
+
+void setup() {
+
+  pinMode(FirstLED, OUTPUT);
+  pinMode(SecLED, OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(FirstLED, HIGH);
+  delay(TimeOn);
+  digitalWrite(FirstLED, LOW);
+  delay(TimeOff);
+
+  digitalWrite(SecLED, HIGH);
+  delay(TimeON);
+  digitalWrite(SecLED, LOW);
+  delay(TimeOFF);
+  
+}
