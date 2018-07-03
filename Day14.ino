c40c0390d7ee70ced2303ef66aa94d5fcfdac06f
DarkTamara <38437336+DarkTamara@users.noreply.github.com>
2018-07-03 17:23:09 +0300

    Add files via upload

---
+++ Day14.ino
@@ -0,0 +1,54 @@
+int RedPin=11;  //set pins for each RGB
+int GreenPin=10;
+int BluePin=9;
+int brightness=255; //sets the brightness of the color
+String ColorChoice; //the user will later on fil this
+
+void setup() {
+  Serial.begin(9600); //turns on serial port
+  
+  pinMode(RedPin, OUTPUT);
+  pinMode(GreenPin, OUTPUT);
+  pinMode(BluePin, OUTPUT);
+
+}
+
+void loop() {
+
+  Serial.println("What color do you want the LED to be ? " ); //ask input
+  Serial.println("Choose from read, blue, green or cyan, please. ");
+  while(Serial.available()==0){}  //waits for input
+  ColorChoice=Serial.readString();  //reads the string from the serial port
+  
+  if(ColorChoice=="red") {
+  analogWrite(RedPin, brightness);  //red
+  analogWrite(BluePin, 0);
+  analogWrite(GreenPin,0);
+  }
+  
+  if(ColorChoice=="green") {
+  analogWrite(RedPin, 0);  
+  analogWrite(BluePin, 0);
+  analogWrite(GreenPin,brightness); //green
+  }
+  
+  if(ColorChoice=="blue") {
+  analogWrite(RedPin, 0);  
+  analogWrite(BluePin, brightness); //green
+  analogWrite(GreenPin,0);
+  }
+
+  if(ColorChoice=="cyan") {
+  analogWrite(RedPin, 0);  
+  analogWrite(BluePin, 255); //cyan
+  analogWrite(GreenPin,255);
+  }
+
+  if(ColorChoice != "red" && ColorChoice !="green" && ColorChoice !="blue" && ColorChoice !="cyan") {
+    Serial.println(" ");
+    Serial.println("WTF?! What even mate ? I said red, green, blue or cyan! Try again, you colorblind banana. ");
+    Serial.println(" ");
+  }
+}
+
+
---
+++ Day16.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>   //loading the servo library
+int pos=0;  //declare and initialise the position variable
+int servoPin=9; //the pin the servo is hooked to
+int servoDelay=25;  //wait 
+int potReading; //the variable from the potentiometer
+int potPin=A0;  
+
+Servo myServo; //create a servo object 
+
+void setup() {  //this will only put it in this position
+
+  Serial.begin(9600);
+  myServo.attach(servoPin); //where the servo to what the servo is attached to
+ 
+}
+
+void loop() {
+  potReading = analogRead(potPin);  //read the value and put in the the variable
+
+  pos = (155./1023.)*potReading +15;  //calculate position based on the potentiometer value
+  myServo.write(pos);   //write the value on the pot to the servo
+  
+}
